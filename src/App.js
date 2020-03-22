ec5ee323ed194fdfa51a9f9e51ae9a0d9d45eb4a
jmylonas <jmylonas@expedia.com>
2020-03-22 14:08:44 +1000

    feat: basic socket server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import openSocket from 'socket.io-client';
 import {OS} from './apps/os';
 
 import './global.scss';
+import { useStores } from './utils/contexts';
+
 
 function App() {
+  const {os} = useStores();
+  
+  useEffect(() => {
+    os.socket = openSocket('http://localhost:8000');
+    os.socket.on('timer', timestamp => console.log(timestamp));
+    os.socket.emit('subscribeToTimer', 1000);
+    return () => os.socket.close();
+  });
 
   return (
     <div className="dandroid-os">
