4da78d80520d7eedd6f0ce2a420d45314c8cafce
Jordaan <jordaanm@gmail.com>
2020-03-24 21:00:21 +1000

    Updates

---
+++ precache-manifest.99a6014216c6f8876bc96a596a24d991.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6de32ad27ff12a9aa349dcfd1dfb70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97e3aba3c9f90ae79ed6",
+    "url": "/static/css/main.3f169cfe.chunk.css"
+  },
+  {
+    "revision": "cc75d710851a2598987c",
+    "url": "/static/js/2.c89ddd23.chunk.js"
+  },
+  {
+    "revision": "0e1d6b6d385c211169de9c34f77c7e52",
+    "url": "/static/js/2.c89ddd23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97e3aba3c9f90ae79ed6",
+    "url": "/static/js/main.34f71dc1.chunk.js"
+  },
+  {
+    "revision": "1ed224db703ab17ea7d3",
+    "url": "/static/js/runtime-main.5f4d225c.js"
+  }
+]);
